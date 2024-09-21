2ee20d757341c2869777d37d7974b919680c89df
Felipe Alves <oliveira.felipe22@escola.pr.gov.br>
2024-09-21 14:25:01 -0300

    Menu lateral finalizado

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -3,5 +3,26 @@
 código da navbar
 
 */
+// criando um seletor
 
-console.log("olá mundo!")
+let menuItem = document.querySelectorAll('.Item-menu')
+
+function selectLink(){
+    menuItem.forEach((item) => {
+        item.classList.remove('ativo')
+    })
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', selectLink)
+})
+
+// Expandir menu
+
+let btnexp = document.querySelector('#btn-exp')
+let menuSide = document.querySelector('.menu-lateral')
+
+btnexp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
